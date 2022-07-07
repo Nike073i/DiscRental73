cc7967d2facecb64b07f90ddbb000e297528a87b
Skuld <nike073i@mail.ru>
2022-07-07 16:45:13 +0400

    Инициализация базовых значений

--- Tests/Common/DiscRental73.DAL.Tests/Data/DbInitializer.cs
+++ Tests/Common/DiscRental73.DAL.Tests/Data/DbInitializer.cs
@@ -10,6 +10,13 @@ namespace DiscRental73.DAL.Tests.Data
             db.DvdDiscs.AddRange(DvdDiscSourceData.GetDvdDiscs());
             db.BluRayDiscs.AddRange(BluRayDiscSourceData.GetBluRayDiscs());
             db.SaveChanges();
+            db.Products.AddRange(ProductSourceData.GetProducts());
+            db.Clients.AddRange(ClientSourceData.GetClients());
+            db.Employees.AddRange(EmployeeSourceData.GetEmployees());
+            db.SaveChanges();
+            db.Rentals.AddRange(RentalSourceData.GetRentals());
+            db.Sells.AddRange(SellSourceData.GetSells());
+            db.SaveChanges();
         }
     }
 }
